0224f9a2a6ba3621001c64a8823639c1fdcfc958
Amir-HShahi <amir13831107h@gmail.com>
2024-11-25 21:01:54 +0330

    done Circle.java

--- 0 - previous exams/others/quiz3/Circle.java
+++ 0 - previous exams/others/quiz3/Circle.java
@@ -1,3 +1,18 @@
-public class Circle {
+public class Circle extends Shape {
+    private int radius;
 
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return radius * radius * 3.14;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * 3.14 * radius;
+    }
+    
 }
