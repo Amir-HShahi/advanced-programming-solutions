43b888082d0fbab9fe4bebc3aafc7bb810789e75
Amir-HShahi <amir13831107h@gmail.com>
2024-11-25 21:15:43 +0330

    done Rectangle.java

--- 0 - previous exams/others/quiz3/Quadrilateral.java
+++ 0 - previous exams/others/quiz3/Quadrilateral.java
@@ -1,5 +1,5 @@
 public abstract class Quadrilateral extends Shape{
-    private double top, bottom, left, right;
+    protected double top, bottom, left, right;
 
     public Quadrilateral(double top, double bottom, double left, double right) {
         this.top = top;
