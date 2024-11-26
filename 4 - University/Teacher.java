28304cc2423d9209d18f734e05f900e73124f332
Amir-HShahi <amir13831107h@gmail.com>
2024-11-26 16:02:03 +0330

    done Teacher.java

--- 4 - University/Teacher.java
+++ 4 - University/Teacher.java
@@ -1,3 +1,16 @@
-public class Teacher {
+public class Teacher extends Staff {
+    private String faculty;
 
+    public Teacher(String firstName, String lastName, int zipCode, int salary, String faculty) {
+        super(firstName, lastName, zipCode, salary);
+        setFaculty(faculty);
+    }
+
+    public String getFaculty() {
+        return this.faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
 }
