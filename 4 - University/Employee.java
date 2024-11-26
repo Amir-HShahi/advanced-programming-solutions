537a9ef7808afb809c784b73bddfcccb9340388b
Amir-HShahi <amir13831107h@gmail.com>
2024-11-26 16:04:58 +0330

    done Employee.java

--- 4 - University/Employee.java
+++ 4 - University/Employee.java
@@ -1,3 +1,17 @@
-public class Employee {
+public class Employee extends Staff {
+    private int workTime = 0;
 
+    public Employee(String firstName, String lastName, int zipCode, int salary, int workTime) {
+        super(firstName, lastName, zipCode, salary);
+        setWorkTime(workTime);
+    }
+
+    public int getWorkTime() {
+        return this.workTime;
+    }
+
+    public void setWorkTime(int workTime) {
+        if (workTime >= 0) 
+            this.workTime = workTime;
+    }
 }
