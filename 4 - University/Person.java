2c4b3a4d22504c4218540ffa56afed9e3811293f
Amir-HShahi <amir13831107h@gmail.com>
2024-11-26 15:28:37 +0330

    done Person.java

--- 4 - University/Person.java
+++ 4 - University/Person.java
@@ -1,4 +1,57 @@
-public class Person {
+public abstract class Person {
+    private static int idTracker = 1000;
+    private final int id;
+    private String firstName, lastName;
+    private String address;
+    private int zipCode;
 
-    
+    protected Person(String firstName, String lastName, String address) {
+        this.id = generateID();
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAddress(address);
+    }
+
+    protected Person(String firstName, String lastName, int zipCode) {
+        this.id = generateID();
+        setFirstName(firstName);
+        setLastName(lastName);
+        setZipCode(zipCode);
+    }
+
+    private static int generateID() {
+        return ++idTracker;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZipCode() {
+        return this.zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
 }
