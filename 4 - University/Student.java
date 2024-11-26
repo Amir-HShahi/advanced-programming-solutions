16ce819b53d6165ffc9901aebee97e58c17f0c6e
Amir-HShahi <amir13831107h@gmail.com>
2024-11-26 15:45:05 +0330

    done Student.java

--- 4 - University/Student.java
+++ 4 - University/Student.java
@@ -1,3 +1,17 @@
-public class Student {
+public class Student extends Person {
+    private static Integer studentNoTracker = 402000;
+    private final String studentNo;
 
+    public Student(String firstName, String lastName, String address) {
+        super(firstName, lastName, address);
+        this.studentNo = generateStudentNo();
+    } 
+    
+    private static String generateStudentNo() {
+        return (++studentNoTracker).toString();
+    }
+
+    public String getStudentNo() {
+        return this.studentNo;
+    }
 }
