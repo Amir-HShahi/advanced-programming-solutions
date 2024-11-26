92a775ca0678744f8995aac320548699e0e8208d
Amir-HShahi <amir13831107h@gmail.com>
2024-11-26 15:56:06 +0330

    done Staff.java

--- 4 - University/Staff.java
+++ 4 - University/Staff.java
@@ -1,3 +1,28 @@
-public class Staff {
+public abstract class Staff extends Person {
+    private static Integer staffNoTracker = 66600;
+    private final String staffNo;
+    private int salary = 0;
 
+    protected Staff(String firstName, String lastName, int zipCode, int salary) {
+        super(firstName, lastName, zipCode);
+        this.staffNo = generateStaffNo();
+        setSalary(salary);
+    }
+
+    private static String generateStaffNo() {
+        return (++staffNoTracker).toString();
+    }
+
+    public String getStaffNo() {
+        return this.staffNo;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary > 0)
+            this.salary = salary;
+    }
 }
